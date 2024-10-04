30c20da58b0da6c8d69d8bcc040b83bef6ce04e8
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-04 13:03:06 +0700

    p

---
+++ BilanganTerbesar2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class BilanganTerbesar2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bil1, bil2, bil3, bilanganBesar = 0;
+
+        System.out.print("Masukkan bilangan 1 = ");
+        bil1 = sc.nextInt();
+        System.out.print("Masukkan bilangan 2 = ");
+        bil2 = sc.nextInt();
+        System.out.print("Masukkan bilangan 3 = ");
+        bil3 = sc.nextInt();
+
+        if (bil1 > bil2 && bil1 > bil3) {
+            bilanganBesar = bil1;
+        } else if (bil2 > bil3) {
+            bilanganBesar = bil2;
+        } else {
+            bilanganBesar = bil3;
+        }
+
+        System.out.println("Bilangan Terbesar   = " + bilanganBesar);
+    }
+}
